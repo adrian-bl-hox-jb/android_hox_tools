4fa7fbaaf515a79babeaba4f1ecfdf126bd8fe36
Adrian Ulrich <adrian@blinkenlights.ch>
2012-12-18 15:32:46 +0100

    include uim-sysfs from TI also moves ugexec into a seperated build dir

---
+++ uim-sysfs/kmpaths.h
@@ -0,0 +1,11 @@
+/* (C) 2012 Adrian Ulrich
+**
+** Define kobject paths for Android
+**
+**/
+
+#define KO_ST_DRV     "/system/lib/modules/st_drv.ko"
+#define KO_BTWILINK   "/system/lib/modules/btwilink.ko"
+#define KO_FM_DRV     "/system/lib/modules/fm_drv.ko"
+#define KO_GPS_DRV    "/system/lib/modules/gps_drv.ko"
+#define KO_FM_V4L     "/system/lib/modules/fm_v4l2_drv.ko"
